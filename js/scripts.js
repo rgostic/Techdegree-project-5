e5c17ee2f082ed5dfaee14e5e749b2b31f0a8305
rgostic <rgostic@outlook.com>
2018-07-27 10:15:06 -0400

    Added styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
    $("#search").on('keyup',function(e) {
-
-
-
        var galleryImages = $(".gallery a");
 
        galleryImages.show();
@@ -11,14 +8,7 @@ $(document).ready(function() {
        var filteredImages = galleryImages.filter(function (c) {
 
             var caption = $(this).data("title");
-            console.log(caption);
-            console.log(searchString);
             return caption.toLowerCase().indexOf(searchString.toLowerCase()) === -1;
        }).hide();
-
-       //filteredImages.each(function () {
-        //   $(this).hide();
-       //})
-
    }) ;
 });
